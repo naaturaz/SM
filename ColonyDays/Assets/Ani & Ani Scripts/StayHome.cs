9351b76e35ee5e8c1b60ebdf2e88c2bf1d179ea0
Manuel Lopez <naaturaz@gmail.com>
2020-03-21 14:33:29 -0400

    Create StayHome.cs

---
+++ ColonyDays/Assets/Ani & Ani Scripts/StayHome.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class StayHome : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void Click()
+    {
+        Application.OpenURL("https://twitter.com/search?q=%23StayHome&src=typeahead_click");
+    }
+}
